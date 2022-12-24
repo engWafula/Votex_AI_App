c23571785da1adfa2f9a4f8f87af0c4f66fcdff0
engWafula <wafulaallan5@gmail.com>
2022-12-24 15:30:42 +0300

    deplyment fixes

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async(e)=>{
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch("https://votex-ai-companion.onrender.com",{
+  const response = await fetch("https://votex.onrender.com",{
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
